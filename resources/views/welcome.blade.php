bb0c0986ba397ec9e35b8af2952523623de8b580
darthchudi <chukwudioranu@ymail.com>
2018-05-16 22:32:25 +0100

    feat: make landing page responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,59 +3,96 @@
 <meta charset="utf-8">
 <meta name="csrf-token" content="{{csrf_token()}}">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Bops</title>
-    <link rel="stylesheet" type="text/css" href="/css/bulma-0.6.2/css/bulma.css">
-    <link rel="stylesheet" type="text/css" href="/css/font awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="/css/bandcamp.css">
-    <link rel="stylesheet" type="text/css" href="/css/wave-modal.css">
-    <link rel="shortcut icon" href="/images/favicon.ico?v=2">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/font awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/dist/css/bootstrap.min.css">
+    <script type="text/javascript" src="css/bootstrap/assets/js/vendor/jquery-slim.min.js"></script>
+    <script type="text/javascript" src="css/bootstrap/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bops-fm.css">
 </head>
-<body>
+<body class="bg-gradient">
     <div id="root">
-        <div class="jumbotron jumbo">
-            <img src="/images/bops.png" class="logo">
-            {{-- <ul>
-                <li> <a href="">About</a> </li>
-                <li> <a href="">Mixes</a> </li>
-                <li> <a href="">Discover</a> </li>
-            </ul> --}}
-            <form @submit.prevent="submit">
-                <div class="field">
-                    <div class="control has-icons-left">
-                        <input type="text" name="url" placeholder="Enter a bandcamp/soundcloud song or album url..." class="input" v-model="link">
-                        <span class="icon is-small is-left">
-                            <i class="fa " :class="linkDetails[1]"></i>
-                        </span>            
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a href="#" class="navbar-brand text-dark">
+                <i class="fa fa-music logo"></i> <span class="logo-tag ml-2"></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle Navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="navbar-item mr-3">
+                        <a href="#" class="nav-link text-dark active">Home</a>
+                    </li>
+
+                    <li class="navbar-item mr-3">
+                        <a href="#" class="nav-link text-dark">About</a>
+                    </li>
+
+                    <li class="text-dark navbar-item">
+                        <a href="#" class="nav-link text-dark">Collab</a>
+                    </li>
+                </ul>
+            </div>  
+        </nav>
+
+        <main class="mt-5">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col">
+                        <form @submit.prevent="submit">
+                            <div class="form-row">
+                                <div class="col-sm col-md-8 container">
+                                    <div class="input-group ml-sm-0 ml-md-5">
+                                        <div class="input-group-prepend rounded-0 bg-white">
+                                            <span class="input-group-text bg-white border-white" id="icon">
+                                                <i class="fa " :class="linkDetails[1]"></i>
+                                            </span>
+                                        </div>
+                                        <input type="text" class="form-control form-control-lg border-0 rounded-0 is-valid" placeholder="Enter a bandcamp or soundcloud link" aria-label="link" aria-describedby="icon" v-model="link" name="url">
+                                        <br/><small class="valid-feedback text-white ml-1 mb-3">It could be a song or album link. We don't mind :) </small>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="col-sm col-md-8 container">
+                                    <input type="submit" name="button" class="ml-sm-0 ml-md-5 container p-custom btn btn-outline-light rounded-0" value="Get bop(s)">
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-
-                <div class="field">
-                    <div class="control">
-                        <input type="submit" name="submit" value="Download the bops" class="submit button is-success is-medium">
-                    </div>
-                </div>
-            </form>
-
-            <loading-modal :status="status" v-if="loading"> </loading-modal>
-            <success-modal :status="successMessage" v-if="success"> </success-modal>
-            <error-modal :status="errorMessage" v-if="error"> </error-modal>
-        </div>
+            </div>
+        </main>
+        <loading-modal :status="status" v-if="loading"> </loading-modal>
+        <success-modal :status="successMessage" v-if="success"> </success-modal>
+        <error-modal :status="errorMessage" v-if="error"> </error-modal>
 
         <song-box v-if="fetchedSong" :song="songDetails"> </song-box>
         <album-box v-if="fetchedAlbum" :album-details="albumDetails" :tracklist="albumTracklist"> </album-box>
+
+
+        <main class="bg-light pt-4 text-center " :class="[!fetchedSong && !fetchedAlbum ? 'fixed-bottom' : '']">
+            <span>
+                Made with 💛 + ☕ + Jollof by <span class="chudi"> Chudi. </span>
+            </span> <br/>
+
+            <span class="ml-2">
+                <a href="https://twitter.com/chudioranu" class="mr-3">
+                    <i class="fa fa-twitter text-primary"></i>
+                </a>
+
+                <a href="https://github.com/darthchudi">
+                    <i class="fa fa-github text-dark"></i>
+                </a>
+            </span> <br/>
+        </main>
     </div>
     
-
-    <footer class="footer">
-        <div class="container">
-            <div class="content has-text-centered">
-              <p class="footer-text">
-                    Made with 💛 and ☕ by  <a href="https://github.com/darthchudi" class="github"> Chudi. </a>
-              </p>
-            </div>
-        </div>
-    </footer>
 
     
 </body>
